a30baaa87149020199bd55207acf2080308d5cd7
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-05-23 22:56:28 -0400

    Tweaked front end

--- leetguide/src/App.js
+++ leetguide/src/App.js
@@ -4,8 +4,11 @@ import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <Card />
+    <div className="App h-screen">
+      <h1 className='mt-5 text-2xl text-left ml-3 font-la logo'>LeetGuide</h1>
+      <header className="App-header">
+        <Card />
+      </header>
     </div>
   );
 }
